5ed3f008fabf4937d49172a131e6180d3b4141f5
gavasquez <gustavovasquez15@gmail.com>
2024-12-19 17:45:50 -0500

    Corrección error servicio

--- src/app/admin/services/page.tsx
+++ src/app/admin/services/page.tsx
@@ -21,7 +21,7 @@ export default async function ServicesPage( { searchParams }: Props ) {
   /* Obtener la configuración del sitio */
   const serviceSettings = await getServiceSettings();
 
-  const { services, totalPages } = await getServices( { page, serviceSettignsId: serviceSettings!.id } ); // revisar
+  const { services, totalPages } = await getServices( { page, serviceSettignsId: serviceSettings?.id! } );
 
 
   return (
@@ -40,7 +40,7 @@ export default async function ServicesPage( { searchParams }: Props ) {
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               {
                 serviceSettings && ( <div className=" text-end">
-                  <Link href={ `/admin/services/${serviceSettings.id}/crear` }
+                  <Link href={ `/admin/services/${ serviceSettings.id }/crear` }
                     className="bg-yellow-500 text-white font-bold py-2 px-4 rounded-md hover:bg-gray-600 hover:text-white transition-all"
                   >
                     Crear un Servicio
