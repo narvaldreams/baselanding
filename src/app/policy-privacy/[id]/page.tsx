8276ea89f22ca44262b2ba51e591c7e4321c2b72
gavasquez <gustavovasquez15@gmail.com>
2024-12-26 10:45:00 -0500

    Footer de politicas de privacidad

--- src/app/policy-privacy/[id]/page.tsx
+++ src/app/policy-privacy/[id]/page.tsx
@@ -1,6 +1,9 @@
+import { getFooter } from '@/actions/auth/footer/get-footer';
 import { getSettingById } from '@/actions/auth/settigns/get-setting-by-id';
-import { TopMenu } from '@/components';
+import { Footer, TopMenu } from '@/components';
 import { PolicyPrivacy } from '@/components/ui/policy-privacy/PolicyPrivacy';
+import Link from 'next/link';
+import { IoIosArrowBack } from 'react-icons/io';
 
 interface Props {
   params: Promise<{ id: string; }>;
@@ -11,6 +14,7 @@ export default async function PolicyPrivacyPage( { params }: Props ) {
   const { id } = await params;
 
   const site = await getSettingById( id );
+  const footer = await getFooter();
 
   return (
     <>
@@ -28,8 +32,29 @@ export default async function PolicyPrivacyPage( { params }: Props ) {
       <div className="container-fluid relative px-40 mt-10">
         <section className="relative md:py-24 py-16">
           <PolicyPrivacy policyPrivacyText={ site?.policyPrivacyText ?? '' } />
+          <div className="flex justify-center items-center mt-5">
+          <Link href="/" className=" text-white font-bold py-2 px-4 rounded-md hover:bg-gray-600 hover:text-white transition-all flex justify-center items-center" style={ { backgroundColor: site?.siteColor || '#000' } }>
+            <IoIosArrowBack size={ 24 } />
+            <span className="ml-1">Regresar</span>
+          </Link>
+          </div>
         </section>
       </div>
+      <Footer
+        id={ site?.id }
+        siteName={ site?.siteName }
+        footer={ footer }
+        logoUrl={ site?.siteLogoUrl }
+        description={ site?.description }
+        facebookUrl={ site?.facebookUrl }
+        twitterUrl={ site?.twitterUrl }
+        instagramUrl={ site?.instagramUrl }
+        linkedinUrl={ site?.linkedinUrl }
+        youtubeUrl={ site?.youtubeUrl }
+        siteColor={ site?.siteColor }
+        siteColorText={ site?.siteColorText }
+        policyPrivacyText={ site?.policyPrivacyText }
+      />
     </>
   );
 }
